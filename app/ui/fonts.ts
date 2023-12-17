336cc6b02b4091773ea6fb76952f4a4ec6361247
Ben <113021701+bensonpen@users.noreply.github.com>
2023-12-17 15:17:12 +0800

    Add files via upload

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,18 +1,19 @@
-<<<<<<< HEAD
 import { Inter, Lusitana } from 'next/font/google';
- 
+
 export const inter = Inter({ subsets: ['latin'] });
 
 export const lusitana = Lusitana({
     weight: ['400', '700'],
     subsets: ['latin'],
-=======
-import { Inter, Lusitana } from 'next/font/google';
- 
-export const inter = Inter({ subsets: ['latin'] });
+});
 
-export const lusitana = Lusitana({
-    weight: ['400', '700'],
-    subsets: ['latin'],
->>>>>>> 77a3cb9 (message)
-  });
+// =======
+// <<<<<<< HEAD
+// import { Inter, Lusitana } from 'next/font/google';
+ 
+// export const inter = Inter({ subsets: ['latin'] });
+
+// export const lusitana = Lusitana({
+//     weight: ['400', '700'],
+//     subsets: ['latin'],
+// >>>>>>> 77a3cb9 (message)
